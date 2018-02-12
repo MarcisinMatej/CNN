154aedd50666309929cd0d39d319cdffd239c5ee
Bc. Matej Marčišin <marcimat@fel.cvut.cz>
2018-02-12 19:59:09 +0100

    Added virtual pictures generator. Need to resolve representaive sample.

--- main.py
+++ main.py
@@ -27,22 +27,13 @@ def RunModel(train_data,train_labels_one_hot,test_data,test_labels_one_hot):
 def RunModelWithGenerators():
     bulk_size = 32
     model_path = 'model/'
-    train_data_dir = 'data/train'
-    validation_data_dir = 'data/validation'
-    nb_train_samples = 2000
-    nb_validation_samples = 800
     n_epochs = 250
     batch_size = 32
     in_shape = (64,64,3)
     model = define_network(in_shape=in_shape)
-    call_back = keras.callbacks.ModelCheckpoint(model_path, verbose=0, save_best_only=False, save_weights_only=True, mode='auto', period=1)
-
-    # model.compile(optimizer='rmsprop', loss=['categorical_crossentropy', 'categorical_crossentropy'],
-    #               loss_weights=[1, 1], metrics=['accuracy'])
-#"categorical_crossentropy", "categorical_crossentropy","categorical_crossentropy", "categorical_crossentropy","categorical_crossentropy"
     opt = optimizers.Adam(lr=0.0000015)
     # model.compile(optimizer=rms, loss=["categorical_crossentropy", "categorical_crossentropy","categorical_crossentropy", "categorical_crossentropy","categorical_crossentropy"], metrics=['accuracy'])
-    model.compile(optimizer=opt,loss= "categorical_crossentropy", metrics=['accuracy'])
+    model.compile(optimizer=opt,loss= "categorical_crossentropy",loss_weights=[1, 1, 1, 1, 1], metrics=['accuracy'])
 
     histories_train = []
     histories_test = []
@@ -103,6 +94,73 @@ def RunLoadedModelWithGenerators():
             c+=1
             histories_test.append(model.evaluate(x=X_test,y=Y_test,batch_size=batch_size))
         plot_history(prepare_eval_history(histories_test), 'epoch_test' + str(e))
+
+
+def RunModelWithVirtualGenerators():
+    bulk_size = 1024
+    model_path = 'model/'
+    train_data_dir = 'data/train'
+    validation_data_dir = 'data/validation'
+    nb_train_samples = 2000
+    nb_validation_samples = 800
+    n_epochs = 250
+    batch_size = 32
+    in_shape = (64,64,3)
+
+    model = load_model(model_path)
+    opt = optimizers.Adam(lr=0.0000015)
+    # model.compile(optimizer=rms, loss=["categorical_crossentropy", "categorical_crossentropy","categorical_crossentropy", "categorical_crossentropy","categorical_crossentropy"], metrics=['accuracy'])
+    model.compile(optimizer=opt,loss= "categorical_crossentropy", metrics=['accuracy'])
+
+    histories_train = []
+    histories_test = []
+
+    from keras.preprocessing.image import ImageDataGenerator
+    datagen = ImageDataGenerator(
+        featurewise_center=True,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=True,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=20,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.2,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.2,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+    #TODO what is representable sample???
+    # datagen.fit(X_sample)  # let's say X_sample is a small-ish but statistically representative sample of your data
+
+    # let's say you have an ImageNet generator that yields ~10k samples at a time.
+    # for e in range(n_epochs):
+    #     print("epoch %d" % e)
+    #     for X_train, Y_train in ImageNet():  # these are chunks of ~10k pictures
+    #         for X_batch, Y_batch in datagen.flow(X_train, Y_train, batch_size=32):  # these are chunks of 32 samples
+    #             loss = model.train(X_batch, Y_batch)
+
+    for e in range(n_epochs):
+        print("epoch %d" % e)
+        tmp = DataGenerator((64, 64), bulk_size)
+        train_gen = tmp.generate_training()
+        # training
+        for X_train, Y_train in train_gen:  # these are chunks of ~bulk pictures
+            for X_batch, Y_batch in datagen.flow(X_train, Y_train, batch_size=32):  # these are chunks of 32 samples
+            #TODO here we can select just 1 attribute for training
+                histories_train.append(model.fit(X_batch, Y_batch, batch_size=batch_size, epochs=1))
+
+        save_model(model,model_path)
+        plot_history(merge_history(histories_train), 'epoch_train' + str(e))
+        # Testing
+        test_gen = tmp.generate_testing()
+        c = 0
+        for X_test, Y_test in test_gen:  # these are chunks of ~bulk pictures
+            print("chunk" + str(c))
+            c+=1
+            histories_test.append(model.evaluate(x=X_test,y=Y_test,batch_size=batch_size))
+        plot_history(prepare_eval_history(histories_test), 'epoch_test' + str(e))
+
+
+
 
 
 if __name__ == "__main__":
