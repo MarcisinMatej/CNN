1483e65b4177d488238ec4962e0d104f3f2e595f
Bc. Matej Marčišin <marcimat@fel.cvut.cz>
2018-02-13 12:57:57 +0100

    Separated testing methods.

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import datetime
-
-import keras
-
 from CNN import *
 from keras import optimizers
 from data_proc.DataGenerator import DataGenerator
 import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
 
 bulk_size = 1024
 model_path = 'model/'
@@ -13,48 +10,6 @@ n_epochs = 250
 batch_size = 32
 in_shape = (64, 64, 3)
 
-
-def bulk_time_test():
-    sizes = [32,128,256,512,1024,2048,4096,8192]
-    res = []
-    for bulk_s in sizes:
-        generator = DataGenerator((64, 64), bulk_s)
-        train_gen = generator.generate_training()
-        # training
-        start = datetime.datetime.now()
-        for X_train, Y_train in train_gen:  # these are chunks of ~bulk pictures
-            break
-        end = datetime.datetime.now()
-        delta = end - start
-        res.append(int(delta.total_seconds() * 1000))
-    import matplotlib.pyplot as plt
-    plt.plot(range(len(sizes)),res,'ro')
-    plt.xticks(range(len(sizes)), sizes)
-    plt.show()
-
-    average = [res[i]/sizes[i] for i in range(len(res))]
-    plt.plot(range(len(sizes)), average,'ro')
-    plt.xticks(range(len(sizes)), sizes)
-    plt.show()
-
-def batch_time_test(model, generator):
-    histories_train = []
-    train_gen = generator.generate_training()
-    res = []
-    sizes = [16,32,48,64,128,256,512]
-    # training
-    for X_train, Y_train in train_gen:  # these are chunks of ~bulk pictures
-        for b_size in sizes:
-            start = datetime.datetime.now()
-            histories_train.append(model.fit(X_train, Y_train, batch_size=b_size, epochs=1))
-            end = datetime.datetime.now()
-            delta = end - start
-            res.append(int(delta.total_seconds() * 1000))
-        break
-    import matplotlib.pyplot as plt
-    plt.plot(range(len(sizes)), res, 'ro')
-    plt.xticks(range(len(sizes)), sizes)
-    plt.show()
 
 def train_model(model,generator,epoch_id):
     histories_train = []
@@ -122,20 +77,6 @@ def RunLoadedModelWithGenerators():
         test_model(model, generator, e)
 
 
-def RunModelBatchTest():
-
-    model = load_model(model_path)
-    opt = optimizers.Adam(lr=0.0000015)
-    model.compile(optimizer=opt,loss= "categorical_crossentropy", metrics=['accuracy'])
-
-    for e in range(n_epochs):
-        print("epoch %d" % e)
-        generator = DataGenerator((64, 64), bulk_size)
-        # Training
-        batch_time_test(model, generator)
-        # Testing
-        test_model(model, generator, e)
-
 
 def RunModelWithVirtualGenerators():
     model = define_network(in_shape=in_shape)
@@ -144,8 +85,6 @@ def RunModelWithVirtualGenerators():
     model.compile(optimizer=opt, loss="categorical_crossentropy", loss_weights=[1, 1, 1, 1, 1], metrics=['accuracy'])
 
     histories_train = []
-
-    from keras.preprocessing.image import ImageDataGenerator
     datagen = ImageDataGenerator(
         featurewise_center=True,  # set input mean to 0 over the dataset
         samplewise_center=False,  # set each sample mean to 0
@@ -194,7 +133,3 @@ if __name__ == "__main__":
     # RunModelWithGenerators()
 
     # RunLoadedModelWithGenerators()
-
-    RunModelBatchTest()
-
-    # bulk_time_test()
---
+++ tests.py
@@ -0,0 +1,88 @@
+import datetime
+
+from CNN import *
+from keras import optimizers
+from data_proc.DataGenerator import DataGenerator
+import tensorflow as tf
+
+
+bulk_size = 1024
+model_path = 'model/'
+n_epochs = 250
+batch_size = 32
+in_shape = (64, 64, 3)
+
+
+def bulk_time_test():
+    sizes = [32,128,256,512,1024,2048,4096,8192]
+    res = []
+    for bulk_s in sizes:
+        generator = DataGenerator((64, 64), bulk_s)
+        train_gen = generator.generate_training()
+        # training
+        start = datetime.datetime.now()
+        for X_train, Y_train in train_gen:  # these are chunks of ~bulk pictures
+            break
+        end = datetime.datetime.now()
+        delta = end - start
+        res.append(int(delta.total_seconds() * 1000))
+    import matplotlib.pyplot as plt
+    plt.plot(range(len(sizes)),res,'ro')
+    plt.xticks(range(len(sizes)), sizes)
+    plt.show()
+
+    average = [res[i]/sizes[i] for i in range(len(res))]
+    plt.plot(range(len(sizes)), average,'ro')
+    plt.xticks(range(len(sizes)), sizes)
+    plt.show()
+
+def batch_time_test(model, generator):
+    histories_train = []
+    train_gen = generator.generate_training()
+    res = []
+    sizes = [16,32,48,64,128,256,512]
+    # training
+    for X_train, Y_train in train_gen:  # these are chunks of ~bulk pictures
+        for b_size in sizes:
+            start = datetime.datetime.now()
+            histories_train.append(model.fit(X_train, Y_train, batch_size=b_size, epochs=1))
+            end = datetime.datetime.now()
+            delta = end - start
+            res.append(int(delta.total_seconds() * 1000))
+        break
+    import matplotlib.pyplot as plt
+    plt.plot(range(len(sizes)), res, 'ro')
+    plt.xticks(range(len(sizes)), sizes)
+    plt.show()
+
+
+def RunModelBatchTest():
+
+    model = define_network(in_shape=in_shape)
+    opt = optimizers.Adam(lr=0.0000015)
+    model.compile(optimizer=opt,loss= "categorical_crossentropy", metrics=['accuracy'])
+
+    for e in range(n_epochs):
+        print("epoch %d" % e)
+        generator = DataGenerator((64, 64), bulk_size)
+        # Training
+        batch_time_test(model, generator)
+
+
+
+if __name__ == "__main__":
+    # issue with memory, in default tensorflow allocates nearly all possible memory
+    # this can result in OOM error later
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+
+    #RunModelBatchTest()
+
+    # bulk_time_test()
+
+    sizes = [16, 32, 48, 64, 128]
+    res = [91,47,40,35,27]
+    plt.plot(range(len(sizes)), res, 'ro')
+    plt.xticks(range(len(sizes)), sizes)
+    plt.show()
